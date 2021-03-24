0f32cb20980862c583d71cbd857a42c2d48b1a54
bija510 <error.zxz@gmail.com>
2021-03-24 10:01:35 -0400

    For test project name change verify

--- src/test/java/playwrightJava/zTemp.java
+++ src/test/java/playwrightJava/zTemp.java
@@ -1,22 +1,43 @@
 package playwrightJava;
 
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserContext;
 import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.ElementHandle;
+import com.microsoft.playwright.JSHandle;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 
 import base.BaseTest;
 
-public class zTemp extends BaseTest{
+public class zTemp {
 
 	Page page;
+
+	@BeforeMethod
+	public void setUp() {
+		Browser browser = Playwright.create().chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+		BrowserContext browserContext = browser.newContext();
+		page = browserContext.newPage();
+	}
+	
 	@Test
 	public void test001() throws InterruptedException {
-		page.navigate("http://demo.automationtesting.in/Datepicker.html");
-		page.click("//button(kf)");
-	}
+		page.navigate("https://www.rahulshettyacademy.com/AutomationPractice/");	
 
+		page.evaluate("document.querySelector('#opentab').click()");
+		// String videoPath = System.getProperty("user.dir")+"\\Data\\Upload file.pdf";
+		 System.out.println(page.video().path());
+		 page.video().path();
+	
+		
+	}
 }
