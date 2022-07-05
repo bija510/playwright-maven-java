1e2ac8e3051f56c95926f5eb70c152c973733aa6
bija chh <error.zxz@gmail.com>
2022-07-05 10:56:13 -0400

    Added Webelment handle, traceViewer & BrowserContext

---
+++ src/test/java/playwrightJava/Test_12_ElementHandleOrWebElementOrLocator.java
@@ -0,0 +1,91 @@
+package playwrightJava;
+
+import java.util.List;
+
+import org.testng.annotations.*;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.ElementHandle;
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class Test_12_ElementHandleOrWebElementOrLocator {
+/***************************************************
+ * WebElement = ElementHandle = Locator
+ * driver.findElements() == page.querySelectorAll()
+ * List<WebElement> == List<ElementHandle>
+ ***************************************************/
+	Page page;
+	BrowserContext browserContext;
+
+	@BeforeMethod
+	public void setUp() {
+		Browser browser = Playwright.create().chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+		browserContext = browser.newContext();
+		page = browserContext.newPage();
+		page.navigate("https://courses.letskodeit.com/practice");
+	}
+
+	@Test
+	public void test_Locator_or_webElement() throws InterruptedException { 
+		Locator openTab = page.locator("text = Open Tab");
+		openTab.click();		
+	}
+	
+	@Test
+	public void test_Locator_or_webElement_2() throws InterruptedException { //table[@id='product']
+		Locator checkBox = page.locator("//input[@type='checkbox']");		
+		int checkBoxs = checkBox.count();
+		System.out.println(checkBoxs);
+		checkBox.first().click();	
+		
+	}
+	
+	@Test
+	public void test_Locators_or_webElements() throws InterruptedException { 
+		Locator tableCells = page.locator("//table[@id='product']//td");	
+		List<String> cellsText = tableCells.allTextContents();
+		
+		//Way : 01
+//		for (int i=0; i<tableCells.count() ; i++) {
+//			String text = tableCells.nth(i).textContent();
+//			System.out.println(text);
+//		}
+//		
+//		//Way : 02
+//		cellsText.forEach(ele -> System.out.println(ele));
+		
+		//Way : 03
+		for(String e : cellsText) {
+			System.out.println(e);
+		}
+		
+			
+	}
+	
+	@Test
+	public void test_getTotalLinks_SizeAndName() throws InterruptedException {
+		List<ElementHandle> eles = page.querySelectorAll("a");
+		System.out.println("total num of links in page:- " + eles.size());
+		
+		for (ElementHandle ele : eles) {
+			System.out.println(ele.innerText());
+		}
+	}
+
+	@Test
+	public void test_ElementHandle() {
+		ElementHandle benzChk = page.querySelector("//input[@id='benzcheck']");
+		benzChk.check();
+	}
+
+	@AfterMethod
+	public void tearDown() {
+//		page.pause();
+		page.close();
+		browserContext.close();
+	}
+}
---
+++ src/test/java/playwrightJava/Test_33_TraceViewer.java
@@ -0,0 +1,40 @@
+package playwrightJava;
+
+import java.nio.file.Paths;
+import org.testng.annotations.Test;
+import com.microsoft.playwright.*;
+
+
+public class Test_33_TraceViewer {
+/****************************************************
+ Description: Capture page action Screenshots, Network Calls & details
+ 1. trace.zip file will downloaded in Project Dir & open in 
+   https://trace.playwright.dev./
+ 2. Or we Can CMD in the Project Dir & Run this command
+ 	mvn exec:java -e -Dexec.mainClass=com.microsoft.playwright.CLI -Dexec.args="show-trace trace.zip"
+ *****************************************************/
+	@Test
+	public void test_traceViewer() {
+		Browser browser = Playwright.create().chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+		BrowserContext browserContext = browser.newContext();
+		
+		//Start tracing before creating / navigating a page
+		browserContext.tracing().start(new Tracing.StartOptions()
+				.setScreenshots(true)
+				.setSnapshots(true));
+		
+		Page page = browserContext.newPage();
+		page.navigate("https://www.rahulshettyacademy.com/AutomationPractice/");
+
+		ElementHandle option1TxtBx = page.querySelector("//input[@id='checkBoxOption1']");
+		option1TxtBx.click();
+		
+		ElementHandle showHideTxtBx = page.querySelector("//input[@id='displayed-text']");
+		showHideTxtBx.click();
+		
+		//Stop tracing and export it into a zip archive
+		browserContext.tracing().stop(new Tracing.StopOptions().setPath(Paths.get("trace.zip")));
+		
+	}
+	
+}
