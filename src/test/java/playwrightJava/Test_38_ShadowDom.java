6f38da6539920e577047d8c49edfcf997b22fa80
bija chh <error.zxz@gmail.com>
2022-07-11 00:30:16 -0400

    Added TC react locator & shadow dom.

---
+++ src/test/java/playwrightJava/Test_38_ShadowDom.java
@@ -0,0 +1,47 @@
+package playwrightJava;
+
+import org.testng.annotations.Test;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class Test_38_ShadowDom {
+
+	/************************************************************
+	  1. We can only automate where Shadow Dom root is OPEN
+	  2. We cannot automate where Shadow Dom root is CLOSE
+	  3. Page --> DOM --> SHadow Dom -->Elements
+	  4. Page --> DOM -->iFrame --> SHadow Dom -->Elements	
+	 *************************************************************/
+	@Test
+	public void test_shadowDome() {
+		Browser browser = Playwright.create().chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));	
+		BrowserContext browserContext = browser.newContext();			
+		Page page = browserContext.newPage();
+		
+		//parent path & the #input is we got it from R-click inspect 
+		page.navigate("https://books-pwakit.appspot.com/");
+		page.locator("book-app[apptitle='BOOKS'] #input").fill("Shadow dom Testing");
+		
+		String message = page.locator("book-app[apptitle='BOOKS'] .books-desc").textContent();
+		System.out.println(message);		
+
+	}
+	
+	@Test
+	public void test_shadowDome_inside_iFrame() {
+		Browser browser = Playwright.create().chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));	
+		BrowserContext browserContext = browser.newContext();			
+		Page page = browserContext.newPage();
+		
+		page.navigate("https://selectorshub.com/xpath-practice-page/");
+		page.frameLocator("#pact").locator("div#snacktime #tea").fill("masala Tea");;
+		
+		page.pause();
+
+	}
+
+}
